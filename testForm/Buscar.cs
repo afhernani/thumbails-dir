f825b45d1246df722557eb3e7980e0f24f6364aa
hernani <hernani@hotmail.com>
2017-03-29 00:28:38 +0100

    funcion workingHard()

--- testForm/Buscar.cs
+++ testForm/Buscar.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ThumbLib;
 
 namespace testForm
 {
@@ -28,6 +30,24 @@ namespace testForm
             //comprobar si existe el directorio en donde buscar.
 
             //los resultados deben ser incluidos en list
+            WorkingHard();
+        }
+        private void WorkingHard()
+        {
+            string what = textBoxWhat.Text;
+            string where = textBoxWhere.Text;
+            if (string.IsNullOrEmpty(what) && string.IsNullOrWhiteSpace(what)) return;
+            if (string.IsNullOrEmpty(where) && string.IsNullOrWhiteSpace(where)) return;
+            if (Directory.Exists(what))
+            {
+                //busqueda comparativa de cada fichero en el directorio
+                //con respecto al otro directorio.
+                string[] lista = Founder.ArrayFilesFromDirectory(what);
+                foreach (var item in lista)
+                {
+                    richTextBox.AppendText(item);
+                }
+            }
         }
     }
 }
