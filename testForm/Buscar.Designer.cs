fcf4332892416a7b682316a06b6c9016b5466449
hernani <hernani@hotmail.com>
2017-03-28 23:44:06 +0100

    Diseno formulario Buscar

---
+++ testForm/Buscar.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace testForm
+{
+    partial class Buscar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Buscar));
+            this.textBoxDir = new System.Windows.Forms.TextBox();
+            this.textBoxDir2 = new System.Windows.Forms.TextBox();
+            this.BtnAction = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxDir
+            // 
+            this.textBoxDir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxDir.Location = new System.Drawing.Point(12, 12);
+            this.textBoxDir.Name = "textBoxDir";
+            this.textBoxDir.Size = new System.Drawing.Size(509, 22);
+            this.textBoxDir.TabIndex = 0;
+            // 
+            // textBoxDir2
+            // 
+            this.textBoxDir2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxDir2.Location = new System.Drawing.Point(12, 40);
+            this.textBoxDir2.Name = "textBoxDir2";
+            this.textBoxDir2.Size = new System.Drawing.Size(509, 22);
+            this.textBoxDir2.TabIndex = 1;
+            // 
+            // BtnAction
+            // 
+            this.BtnAction.BackColor = System.Drawing.Color.Yellow;
+            this.BtnAction.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAction.Image = global::testForm.Properties.Resources.Ant_52px;
+            this.BtnAction.Location = new System.Drawing.Point(428, 285);
+            this.BtnAction.Name = "BtnAction";
+            this.BtnAction.Size = new System.Drawing.Size(93, 27);
+            this.BtnAction.TabIndex = 3;
+            this.BtnAction.Text = "O.K";
+            this.BtnAction.UseVisualStyleBackColor = false;
+            this.BtnAction.Click += new System.EventHandler(this.BtnAction_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(15, 292);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(70, 17);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Directorio";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox.Location = new System.Drawing.Point(14, 71);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(506, 208);
+            this.richTextBox.TabIndex = 5;
+            this.richTextBox.Text = "";
+            // 
+            // Buscar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 322);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.BtnAction);
+            this.Controls.Add(this.textBoxDir2);
+            this.Controls.Add(this.textBoxDir);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Buscar";
+            this.Text = "Buscar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxDir;
+        private System.Windows.Forms.TextBox textBoxDir2;
+        private System.Windows.Forms.Button BtnAction;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RichTextBox richTextBox;
+    }
+}
---
+++ testForm/Buscar.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testForm
+{
+    public partial class Buscar : Form
+    {
+        public Buscar()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAction_Click(object sender, EventArgs e)
+        {
+            // si ratio-boton esta abilitado es un directorio
+            // el label dir de cabeza, si no es un fichero.
+
+            //comprobaremos si existe el fichero o el directorio
+            //que se pretende busacar.
+
+            //comprobar si existe el directorio en donde buscar.
+
+            //los resultados deben ser incluidos en list
+        }
+    }
+}
