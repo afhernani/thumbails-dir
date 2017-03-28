d24be2ad8a7aa63c06c32ee4907400dc625b07d0
hernani <hernani@hotmail.com>
2017-03-28 21:59:52 +0100

    Crea componente Founder and proyect console testFound

---
+++ ThumbLib/Founder.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThumbLib
+{
+    public partial class Founder : Component
+    {
+        public Founder()
+        {
+            InitializeComponent();
+        }
+
+        public Founder(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();  
+        }
+        /// <summary>
+        /// delegado genericoa implement interfaz IEnumerable
+        /// </summary>
+        public delegate IEnumerable getChilds<T>(T element);
+        /// <summary>
+        /// Función generica de busqueda implement interfaz IEnumerable
+        /// se obtiene los nodos de busqueda IEnumerable (elementos) por
+        /// que los elementos sólo implementan la interfaz no generiaca.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="parent">Elemento contenedor</param>
+        /// <param name="elements">coleccion de elementos implementan IEnumerable</param>
+        /// <param name="criterial">logica del criterio de busqueda</param>
+        /// <returns></returns>
+        IEnumerable<T> FindRescursive<T>(T parent, getChilds<T> elements, Predicate<T> criterial)
+        {
+            foreach (T element in elements.Invoke(parent))
+            {
+                if (criterial.Invoke(element))
+                    yield return element;
+                foreach (T element2 in FindRescursive<T>(element, elements, criterial))
+                {
+                    yield return element2;
+                }
+            }
+        }//end FindRecursive.
+        /// <summary>
+        /// Criterio de busqueda bajo la definicion de delegado.
+        /// </summary>
+        readonly Predicate<DirectoryInfo> LookForDirectory = (DirectoryInfo D) => D is DirectoryInfo;
+        /// <summary>
+        /// este delegado define como se obtiene los subdirectorios.
+        /// </summary>
+        readonly getChilds<DirectoryInfo> getChildDirectorys = (DirectoryInfo D)=> D.GetDirectories();
+
+        public void SearchFileinDirectory(DirectoryInfo Dir, FileInfo file)
+        {
+            foreach (DirectoryInfo element in FindRescursive<DirectoryInfo>(Dir, getChildDirectorys, LookForDirectory))
+            {
+                Debug.WriteLine(element.Name);
+            }
+        }
+    }
+}
---
+++ testFound/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThumbLib;
+
+namespace testFound
+{
+    
+    class Program
+    {
+        /// <summary>
+        /// multi tarea con ventanas windows
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // Test if input arguments were supplied:
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("Please enter a Directory and Name File");
+                System.Console.WriteLine(" Path Name-File");
+                return;
+            }
+            if (args.Length == 2)
+            {
+                System.Console.WriteLine($"Argumentos pasados :=> {args.Length}");
+                System.Console.WriteLine($"DirectoryInfo :=> {args[0]} \n FileInfo :=> {args[1]}");
+                if (Directory.Exists(args[0]) && !String.IsNullOrEmpty(args[1]))
+                {
+                    Founder found = new Founder();
+                    found.SearchFileinDirectory(new DirectoryInfo(args[0]), new FileInfo(args[1]));
+                }
+            }
+            //while (!Tar)
+            //{
+            //    //nada.
+            //}
+            //esperamos para salir.
+            System.Console.ReadKey();
+        }
+    }
+}
