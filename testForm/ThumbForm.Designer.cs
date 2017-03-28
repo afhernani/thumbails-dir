c60e42c5285c6ba61567726523fdb1be636975c9
hernani <hernani@hotmail.com>
2017-03-28 23:22:34 +0100

    añadido boton buscar

--- testForm/ThumbForm.Designer.cs
+++ testForm/ThumbForm.Designer.cs
@@ -34,6 +34,7 @@
             this.listBoxTareas = new System.Windows.Forms.ListBox();
             this.labeltex = new System.Windows.Forms.Label();
             this.labelIndex = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -48,7 +49,7 @@
             // 
             // BtnTask
             // 
-            this.BtnTask.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.BtnTask.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.BtnTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnTask.Location = new System.Drawing.Point(566, 12);
@@ -76,7 +77,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.labeltex.AutoSize = true;
             this.labeltex.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labeltex.Location = new System.Drawing.Point(570, 52);
+            this.labeltex.Location = new System.Drawing.Point(566, 43);
             this.labeltex.Name = "labeltex";
             this.labeltex.Size = new System.Drawing.Size(55, 16);
             this.labeltex.TabIndex = 4;
@@ -89,17 +90,30 @@
             this.labelIndex.AutoSize = true;
             this.labelIndex.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.labelIndex.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelIndex.Location = new System.Drawing.Point(600, 79);
+            this.labelIndex.Location = new System.Drawing.Point(619, 59);
             this.labelIndex.Name = "labelIndex";
             this.labelIndex.Size = new System.Drawing.Size(40, 24);
             this.labelIndex.TabIndex = 5;
             this.labelIndex.Text = "000";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(566, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 28);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Buscar";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // ThumbForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(671, 133);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.labelIndex);
             this.Controls.Add(this.labeltex);
             this.Controls.Add(this.listBoxTareas);
@@ -121,6 +135,7 @@
         private System.Windows.Forms.ListBox listBoxTareas;
         private System.Windows.Forms.Label labeltex;
         private System.Windows.Forms.Label labelIndex;
+        private System.Windows.Forms.Button button1;
     }
 }
 
